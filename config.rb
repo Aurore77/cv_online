428e861067cc3d2c884cd715a6f9d0f3ad67014b
David Aurore <aurore_david77@hotmail.fr>
2017-03-22 17:01:31 +0100

    remise en place fichier sass/compass

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+css_dir = "/src/CvBundle/Resources/public/stylesheets"
+sass_dir = "/src/CvBundle/Resources/public/sass"
+images_dir = "/src/CvBundle/Resources/public/images"
+javascripts_dir = "/src/CvBundle/Resources/public/javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
